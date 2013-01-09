97da2996d20b0ee073b458b793afc9e2d75d2177
Justin Appler <jappler@salesforce.com>
2013-01-08 16:50:00 -0800

    Initial commit of cpdown files

---
+++ copied_files.rb
@@ -0,0 +1,34 @@
+class CopiedFiles
+	@copied = nil
+    @copiedFilename = nil
+    
+    def initialize(filename)
+        @copiedFilename = filename
+        begin
+            @copied = IO.readlines(@copiedFilename).collect { |line| line.chomp }
+            $log.info('Opened Copied File list, Size: ' + @copied.length.to_s)
+        rescue
+            @copied = Array.new()
+            $log.warn('No Copied File list found, starting empty')
+        end
+	end
+    
+    def add(file)
+        @copied.push file
+    end
+    
+    def exists(file)
+        @copied.include? file
+    end
+    
+    def save
+        begin
+            f = File.open(@copiedFilename, 'w')
+            @copied.each { |line| f.write(line + "\n") }
+            f.close
+            $log.info('Saved the copied file list')
+        rescue
+            $log.error('Couldn\'t save the Copied Files list')
+        end
+    end
+end
---
+++ cpdown.rb
@@ -0,0 +1,15 @@
+require 'logger'
+require 'copied_files'
+
+# Constants
+COPIED = "copied"
+LOGFILE = "log"
+
+# Logging
+$log = Logger.new(LOGFILE, File::WRONLY | File::APPEND)
+$log.level = Logger::INFO
+
+# CopiedFile list
+copied = CopiedFiles.new(COPIED)
+
+
